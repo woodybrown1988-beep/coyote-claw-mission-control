37619199a3c90542f06ef39cab2abc985324f457
woodybrown1988-beep <woodybrown1988@gmail.com>
2026-06-05 20:49:50 +0100

    coder: job fb0ffd35-58af-423f-b822-18bece53fbd3 (#17)
    Co-authored-by: coyote-claw worker <worker@coyote-claw.local>

--- test/format-job-age.test.js
+++ test/format-job-age.test.js
@@ -8,10 +8,14 @@ const { formatCount, formatJobAge, renderDashboard } = require('../mission-contr
 test('formatJobAge formats representative age ranges', () => {
   const renderedAt = 1_700_000_000_000;
 
-  assert.equal(formatJobAge(renderedAt - 30 * 1000, renderedAt), '0m');
+  assert.equal(formatJobAge(renderedAt - 30 * 1000, renderedAt), 'just now');
+  assert.notEqual(formatJobAge(renderedAt - 30 * 1000, renderedAt), '0m');
+  assert.equal(formatJobAge(renderedAt - 60 * 1000, renderedAt), '1m');
   assert.equal(formatJobAge(renderedAt - 12 * 60 * 1000, renderedAt), '12m');
   assert.equal(formatJobAge(renderedAt - 3 * 60 * 60 * 1000, renderedAt), '3h');
   assert.equal(formatJobAge(renderedAt - 9 * 24 * 60 * 60 * 1000, renderedAt), '9d');
+  assert.equal(formatJobAge(renderedAt - 31 * 24 * 60 * 60 * 1000, renderedAt), '2023-10-14 22:13:20 UTC');
+  assert.equal(formatJobAge(renderedAt + 1000, renderedAt), '-');
 });
 
 test('formatCount formats singular and plural count labels', () => {
